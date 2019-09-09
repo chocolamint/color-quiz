ceef669279ae96a0d286dbc3197e50fa257c5ec2
しゃるしぃ <info@angelbell.jp>
2019-09-09 11:40:08 +0900

    index.scss のうち App.tsx に関係するものは App.scss にわける

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
+import './App.scss';
 import './quiz/WhichCode.scss';
 import './quiz/WhichColor.scss';
 import App from './App';
