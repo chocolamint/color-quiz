95e98319f5fd92dd052df1601326a2e4b93850e9
しゃるしぃ <info@angelbell.jp>
2019-09-10 11:38:00 +0900

    ランダム性を確かめるためシード違いの2ケースでテストする

--- src/__tests__/Utils.test.ts
+++ src/__tests__/Utils.test.ts
@@ -2,11 +2,13 @@ import { shuffle, Random } from "../Utils";
 
 describe("shuffle", () => {
 
-    it("shuffle array", () => {
+    const array = [1, 2, 3, 4];
 
-        const array = [1, 2, 3, 4];
-        const actual = shuffle(array, new Random(88675123));
-
-        expect(actual).toEqual([2, 4, 1, 3]);
+    test.each([
+        [88675123, [2, 4, 1, 3]],
+        [64184574, [3, 4, 2, 1]],
+    ])("shuffle array (seed: %i)", (seed, expected) => {
+        const actual = shuffle(array, new Random(seed as number));
+        expect(actual).toEqual(expected);
     });
 });
