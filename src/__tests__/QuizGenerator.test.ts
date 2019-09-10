e0e8f229ad7f2ecbfe5eb27fd054f6ed19ea8478
しゃるしぃ <info@angelbell.jp>
2019-09-10 15:18:06 +0900

    ColorSchemeQuiz のテストを追加

--- src/__tests__/QuizGenerator.test.ts
+++ src/__tests__/QuizGenerator.test.ts
@@ -3,18 +3,44 @@ import { createQuizGenerator } from "../quiz/QuizGenerator";
 
 describe("createQuizGenerator", () => {
 
-    test("returns ColorChoiceQuiz", () => {
+    describe("ColorChoiceQuiz", () => {
 
-        const random = new Random(0);
-        const generator = createQuizGenerator(random);
+        test("returns quiz", () => {
 
-        const quiz = generator("ColorChoiceQuiz");
+            const random = new Random(0);
+            const generator = createQuizGenerator(random);
 
-        expect(quiz).toEqual(expect.anything());
-        expect(quiz.type).toEqual("ColorChoiceQuiz");
-        expect(quiz.subType).toEqual(expect.any(String));
-        expect(quiz.question).toEqual(expect.any(String));
-        expect(quiz.choices).toEqual(expect.any(Array));
-        expect(quiz.answer).toEqual(expect.any(Number));
+            const quiz = generator("ColorChoiceQuiz");
+
+            expect(quiz).toEqual(expect.anything());
+            expect(quiz.type).toBe("ColorChoiceQuiz");
+            if (quiz.type !== "ColorChoiceQuiz") throw Error(); // asserts ;-(
+            expect(quiz.subType).toEqual(expect.any(String));
+            expect(quiz.question).toEqual(expect.any(String));
+            expect(quiz.choices).toEqual(expect.any(Array));
+            expect(quiz.choices[0]).toHaveProperty("pccsCode");
+            expect(quiz.answer).toEqual(expect.any(Number));
+        });
+    })
+
+    describe("ColorSchemeQuiz", () => {
+
+        test("returns quiz", () => {
+
+            const random = new Random(0);
+            const generator = createQuizGenerator(random);
+
+            const quiz = generator("ColorSchemeQuiz");
+
+            expect(quiz).toEqual(expect.anything());
+            expect(quiz.type).toBe("ColorSchemeQuiz");
+            if (quiz.type !== "ColorSchemeQuiz") throw Error(); // asserts ;-(
+            expect(quiz.subType).toEqual(expect.any(String));
+            expect(quiz.question).toEqual(expect.any(String));
+            expect(quiz.choices).toEqual(expect.any(Array));
+            expect(quiz.choices[0]).toEqual(expect.any(Array));
+            expect(quiz.choices[0][0]).toHaveProperty("pccsCode");
+            expect(quiz.answer).toEqual(expect.any(Number));
+        });
     });
 });
